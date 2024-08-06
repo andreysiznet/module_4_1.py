c7866306bc852e1adee5212591504ab1e78a33b2
andreysiznet <andriktatkov@gmail.com>
2024-08-06 17:13:35 +0300

    Create module_4_1.py

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as divf
+from true_math import divide as divt
+
+
+
+result1 = divf(69, 3)
+result2 = divf(3, 0)
+result3 = divt(49, 7)
+result4 = divt(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
